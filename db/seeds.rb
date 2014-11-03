3d0d2cdec17cc35b44dc2d9e9b73ab7e757fcbf0
Quinton Dang <qdang16@berkeley.edu>
2014-11-03 00:32:44 -0800

    New seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,24 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Make Pokemon
-%w(Squirtle Charmander Bulbasaur Pikachu).each do |name|
-  Pokemon.create name: name, level: rand(1..20), health: 100
+# # Make Pokemon
+# %w(Squirtle Charmander Bulbasaur Pikachu).each do |name|
+#   Pokemon.create name: name, level: rand(1..20), health: 100
+# end
+
+%w(Bulbasaur Ivysaur Venusaur Charmander Charmeleon Charizard Squirtle Wartortle Blastoise Caterpie Metapod Butterfree Weedle Kakuna Beedrill Pidgey Pidgeotto Pidgeot Rattata Raticate Spearow Fearow Ekans Arbok Pikachu Raichu Sandshrew Sandslash Nidoran Nidorina Nidoqueen Nidoran Nidorino Nidoking Clefairy Clefable Vulpix Ninetales).each do |name|
+  Pokemon.create name: name, level: rand(1..100), health: 100
+end
+
+%w(Jigglypuff Wigglytuff Zubat Golbat Oddish Gloom Vileplume Paras Parasect Venonat Venomoth Diglett Dugtrio Meowth Persian Psyduck Golduck Mankey Primeape Growlithe Arcanine Poliwag Poliwhirl Poliwrath Abra Kadabra Alakazam Machop Machoke Machamp Bellsprout Weepinbell Victreebel Tentacool Tentacruel Geodude Graveler Golem Ponyta Rapidash Slowpoke Slowbro Magnemite Magneton Farfetch'd Doduo Dodrio Seel Dewgong).each do |name|
+  Pokemon.create name: name, level: rand(1..100), health: 100
+end
+
+%w(Grimer Muk Shellder Cloyster Gastly Haunter Gengar Onix Drowzee Hypno Krabby Kingler Voltorb Electrode Exeggcute Exeggutor Cubone Marowak Hitmonlee Hitmonchan Lickitung Koffing Weezing Rhyhorn Rhydon Chansey Tangela Kangaskhan Horsea Seadra Goldeen Seaking Staryu Starmie Mr. Mime Scyther Jynx Electabuzz Magmar Pinsir Tauros Magikarp Gyarados Lapras Ditto Eevee Vaporeon Jolteon Flareon Porygon Omanyte Omastar Kabuto Kabutops Aerodactyl Snorlax Articuno Zapdos Moltres Dratini Dragonair Dragonite Mewtwo Mew).each do |name|
+  Pokemon.create name: name, level: rand(1..100), health: 100
 end
 
 # Make other trainers
-%w(Ash Gary Misty Brock).each do |name|
-  Trainer.create name: name, email: name+"@pokeportal.com", password: 'password'
-end
+# %w(Ash Gary Misty Brock).each do |name|
+#   Trainer.create name: name, email: name+"@pokeportal.com", password: 'password'
+# end
